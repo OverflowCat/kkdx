5eff0f219d0053233af7654969c2a216237a99d0
Glitch (kkdx) <none>
2020-05-16 14:19:52 +0000

    🚝⛳️ Meticulous output: tags, native pic and more

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require("telegraf");
 const fetch = require("node-fetch");
+const pangu = require("pangu");
 function tag(cont, htag) {
   return "<" + htag + ">" + cont + "</" + htag + ">";
 }
@@ -22,21 +23,60 @@ function mov(id, done) {
       console.log(d);
       var rating = d.rating.average;
       var res = tag(d.title, "b");
-      if (d.hasOwnProperty("aka"))
-        res += tag(" ( " + d.aka.join(" · ") + " ) ", "i");
-      res += "\n" + d.summary.replace("©豆瓣", "");
+      if (d.hasOwnProperty("aka")) {
+        var locations = ["(港)", "(台)"];
+        if (d.aka != "") {
+          var alias = d.aka.map(ele => {
+            var alia;
+            locations.map(area => {
+              if (ele.indexOf(area) >= 1) {
+                alia = ele.replace(area, " " + area);
+              } else {
+                alia = ele;
+              }
+            });
+            if (ele.indexOf(" ") >= 0) {
+              alia = alia.split(" ");
+              alia[0] = tag(alia[0], "code");
+              alia = alia.join(" ");
+              console.log(alia);
+            }
+            return alia;
+          });
+          res += " ( " + alias.join(" · ") + " ) ";
+        }
+      }
+      var tags = d.tags.map(_tag => {
+        _tag = _tag.split("·").join("");
+        return /[0-9]{4}/.test(_tag) ? _tag + "年" : _tag;
+      });
+      if (d.original_title != "") res += "\n" + tag(d.original_title, "i");
+      var languages = d.languages.map(lang => {
+        return "#" + lang;
+      });
+      res += "\n" + tag(languages.join("、"), "b");
+      res += " #" + tags.join(" #");
       res +=
         "\n" +
-        tag("上映时间 ", "b") +
-        "\n" +
-        tag(d.pubdates.join("\n"), "code");
+        pangu.spacing(
+          d.summary
+            .replace("©豆瓣", "")
+            .split("“")
+            .join("「")
+            .split("”")
+            .join("」")
+        );
+      res += "\n" + tag("上映时间 ", "b");
+      res +=
+        d.pubdates.length > 1
+          ? "\n" + tag(d.pubdates.join("\n"), "code")
+          : tag(d.pubdates.join("\n"), "code");
 
-      res += "\n豆瓣评分 " + rating + "\n" + d.images.large;
-      // "nhttps://m.douban.com/movie/subject/" + id;
-      //      d.iamges.large;
-      //     d.pubdates;
-
-      done(res);
+      if (rating != 0) res += "\n<b>豆瓣评分 " + rating + " </b>";
+      if (d.hasOwnProperty("comments_count"))
+        res += "  （ " + tag(d.comments_count + " 条评论）", "i");
+      //res = pangu.spacing(res);
+      done([res, d.images.large]);
     })
     .catch();
 }
@@ -46,6 +86,18 @@ bot.start(ctx => ctx.reply("Welcome!"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
 bot.on("sticker", ctx => ctx.reply("👍"));
 bot.hears("hi", ctx => ctx.reply("Hey there"));
+
+function sendPhoto(chatId, photo, caption, extra) {
+  bot.telegram.callApi(
+    "sendPhoto",
+    Object.assign(
+      { chat_id: chatId, photo: photo },
+      { caption: caption },
+      extra
+    )
+  );
+  return console.log("Sent.");
+}
 
 bot.on("message", ctx => {
   var ren = ctx.message.from.id.toString();
@@ -57,64 +109,84 @@ bot.on("message", ctx => {
     return ctx.reply("[你无权使用]");
   //return 1;
 
-  if (ctx.message.hasOwnProperty("entities")) {
-    var úrl = ctx.message.entities[0].url;
+  const aboutMenu = Telegraf.Extra.markdown().markup(m =>
+    m.keyboard([m.callbackButton("⬅️ Back")]).resize()
+  );
+
+  if (
+    ctx.message.hasOwnProperty("caption_entities") ||
+    ctx.message.hasOwnProperty("entities")
+  ) {
+    var úrl = ctx.message.hasOwnProperty("entities")
+      ? ctx.message.entities[0].url
+      : ctx.message.caption_entities[0].url;
     var downbtn = Telegraf.Extra.HTML().markup(m =>
       m.inlineKeyboard([
-        m.urlButton("🗂️下载⬇️", úrl),
-        m.urlButton("✨进群➕", "t.me/PanoanDriveBasic")
-      ])
-    );
-
-    const aboutMenu = Telegraf.Extra.markdown().markup(m =>
-      m.keyboard([m.callbackButton("⬅️ Back")]).resize()
-    );
-  }
-
-  if (ctx.message.hasOwnProperty("caption_entities")) {
-    var úrl = ctx.message.caption_entities[0].url;
-    var downbtn = Telegraf.Extra.HTML().markup(m =>
-      m.inlineKeyboard([
-        m.urlButton("🗂️下载⬇️", úrl),
-        m.urlButton("✨进群➕", "t.me/PanoanDriveBasic")
+        [m.urlButton("🗂️在 Drive 中查看⬇️", úrl)],
+        [
+          m.urlButton("🈂️服务器升级⏫", "https://t.me/PanoanDriveBasic/44470/"), //"t.me/PanoanDriveBasic"),
+          m.urlButton("萌猫大佬的视频站", "http://moetv.live")
+        ]
       ])
     );
   }
   var t = ctx.message.hasOwnProperty("caption")
     ? ctx.message.caption
     : ctx.message.text;
+  var originaltext = t;
   t = t.replace(/\(.*\)/g, "");
   t = t.replace(/\[.*\]/g, " ").trim();
   if (true) {
-    t = t.replace("\n", ".");
+    t = t.split(" ").join(".");
+    t = t.split("-").join(".");
+    t = t.split("\n").join(".");
     t = t.split(".");
+    console.log(t);
     var i, T;
     for (i = 0; i < t.length; i++) {
       if (t[i] == undefined) continue;
-      if (/s[0-9]+/.test(t[i])) break;
-      if (/(1080|2160)p?/g.test(t[i])) break;
-      if (!/[a-zA-Z]+/.test(t[i])) continue;
+      if (/((s|S)[0-9]+|Season)/.test(t[i])) break; //季
+      if (/[0-9]+/.test(t[i]) && i >= 1) break; //防止影片的名称是数字
+      if (/((1080|2160)p?|blueray|x264)/gi.test(t[i])) break;
+      if (!/[a-zA-Z0-9]+/.test(t[i])) continue;
       T += t[i] + " ";
     }
   } else {
     var T = t;
   }
   var t = T
-    //.join(" ")
-    .replace("Shareable link: here", "")
-    .replace(",", " ")
-    //.replace("undefined")
-    .trim();
+    ? T.replace("Shareable link: here", "")
+        .replace(",", " ")
+        .trim()
+    : "";
   console.log("__" + t + "__");
   t = t.replace("undefined", "");
   ctx.reply("作品名称：" + t);
 
   search(encodeURIComponent(t), id => {
     if (id == "NF") return ctx.reply("Not found.");
-    mov(id, d => {
+    mov(id, response => {
+      var d = response[0];
+      var picurl = response[1];
+      var d = [
+        d,
+        tag("文件名", "b"),
+        tag(
+          originaltext
+            .split("\n\n")
+            .join("\n")
+            .replace("Shareable link: here", ""),
+          "code"
+        )
+      ].join("\n");
+      console.log(
+        "==========reply msg:" + d + "=============end reply============="
+      );
       if (downbtn) {
-        ctx.reply(d, downbtn);
-        return bot.telegram.sendMessage("@Panoan4K", d, downbtn);
+        ctx.reply(d + "\n" + picurl, downbtn);
+        //return bot.telegram.sendMessage("@Panoan4K", d, downbtn);
+        sendPhoto("@Panoan4K", picurl, d, downbtn);
+        return console.log("done.");
       } else {
         return ctx.reply(d);
       }
